606cbb769e121147cebb06917d713fdc8b18eb1e
Patrick Hudgings <phudgings@gmail.com>
2026-05-12 18:18:23 -0400

    Troubleshooting unit test errors

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,13 +3,14 @@ from src.main import app
 
 client = TestClient(app)
 
+
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json()["status"] == "Online"
 
+
 def test_predict_endpoint():
-    # Replace with a course name that exists in your model
     test_data = {
         "CourseName": "Mount Bradford Preserve",
         "Month": 5,
@@ -17,5 +18,7 @@ def test_predict_endpoint():
         "Duration_Min": 120.0
     }
     response = client.post("/predict", json=test_data)
+    # If the model file is missing on GitHub (which it will be), 
+    # this will return a 500 or 404 error.
     assert response.status_code == 200
     assert "predicted_total_score" in response.json()
